c929f321cfcb02b3ba965921a8cd7bdccd431956
MWPX777 <mwpx777@gmail.com>
2021-02-05 13:52:44 -0600

    finished with 9.2

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-var profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => {
-    for (let i=0; i < profileDataArr.length; i++){
-    console.log(profileDataArr[i]);
-}
+const fs = require('fs'); 
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+const [name, github] = profileDataArgs;
+
+fs.writeFile('./index.html', generatePage(name, github), err=>{
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!')
+});
 
 
-console.log('================');
-
-profileDataArr.forEach(profileItem => console.log(profileItem));
 
 
-};
 
-printProfileData(profileDataArgs);
+
+
+
