902b65fc9a9df650575760ee0084e5f889890bbd
MWPX777 <mwpx777@gmail.com>
2021-02-06 13:38:13 -0600

    finished with 9.3

--- app.js
+++ app.js
@@ -17,17 +17,46 @@ const promptUser = () => {
       {
         type: 'input',
         name: 'name',
-        message: 'What is your name?'
+        message: 'What is your name? (Required)',
+        validate: nameInput =>{
+            if (nameInput){
+                return true;
+            }else{
+                console.log('Please enter your name!');
+                return false;
+            }
+        }
       },
       {
         type: 'input',
         name: 'github',
-        message: 'Enter your GitHub Username'
+        message: 'Enter your GitHub Username (Required)',
+        validate: githubInput =>{
+            if (githubInput){
+                return true;
+            }else{
+                console.log('Please enter your GitHub Username!');
+                return false;
+            }
+        }
+      },
+      {
+          type: 'confirm',
+          name: 'confirmAbout',
+          message: 'Would you like to enter some information about yourself for an "About" section?',
+          default: true
       },
       {
         type: 'input',
         name: 'about',
-        message: 'Provide some information about yourself:'
+        message: 'Provide some information about yourself:',
+        when: ({confirmAbout}) =>{
+            if (confirmAbout){
+                return true;
+            }else{
+                return false;
+            }
+        }
       }
     ]);
   };
@@ -51,13 +80,29 @@ return inquirer
         {
             type: 'input',
             name: 'name',
-            message: 'What is the name of your project?',
+            message: 'What is the name of your project? (Required)',
+            validate : projectName =>{
+                if(projectName){
+                    return true;
+                }else{
+                    console.log('Please enter a project name!');
+                    return false;
+                }
+            }
         
         },
         {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of your project (Required)'
+            message: 'Provide a description of your project (Required)',
+            validate: projectDescription =>{
+                if (projectDescription){
+                    return true;
+                }else{
+                    console.log('Please enter a description!');
+                    return false;
+                }
+            }
         },
         {
             type: 'checkbox',
@@ -68,7 +113,15 @@ return inquirer
         {
             type: 'input',
             name: 'link',
-            message: 'Enter the GitHub link to your project (Required)'
+            message: 'Enter the GitHub link to your project (Required)',
+            validate: githubLink =>{
+                if(githubLink){
+                    return true;
+                }else{
+                    console.log('Please enter a GitHub link!');
+                    return false;
+                }
+            }
         },
         {
             type: 'confirm',
